92acfb8849d25d75a163ff115344647a89f41074
nilaoutbound <nilaoutbound@gmail.com>
2025-12-09 12:36:38 +0700

    Add service worker for caching assets
    Implement service worker for caching files.

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = 'nila-dashboard-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
 
