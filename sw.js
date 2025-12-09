1ffccb037d910033c2e465de4e09f27f548c8d94
nilaoutbound <nilaoutbound@gmail.com>
2025-12-09 13:26:57 +0700

    Add caching for service worker install and fetch events
    Implement caching strategy for service worker installation and fetch events.

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
 self.addEventListener('install', e => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open('nila-v1').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './nila-192.png',
+        './nila-512.png'
+      ])
+    )
+  );
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
