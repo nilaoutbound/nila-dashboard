3a0191d4a3057ff8ad7bc502877591236aef8e55
nilaoutbound <nilaoutbound@gmail.com>
2025-12-09 13:10:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,5 @@
-const CACHE_NAME = 'nila-dashboard-v1';
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
-
+self.addEventListener('fetch', () => {});
